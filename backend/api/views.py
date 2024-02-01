064c3114f1db14e258f7744040b0bd6e25b5a9b5
Ahmad Ali <ahmadalibaffa@gmail.com>
2024-02-02 02:00:34 +0500

    Echo GET data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,18 @@
 from django.http import JsonResponse
-
-def api_home(request):
-    return JsonResponse({"message": "This is your django API response"})
+import json
+def api_home(request, *args, **kwargs):
+    #this request is HttpRequest from Django
+    # print(dir(request))
+    print(request.GET)
+    body = request.body #byte string of JSON data
+    # now convert tht data from byte string to json
+    data = {}
+    try:
+        data = json.loads(body) # takes string of json data and turns into Python Dictionary
+    except:
+        pass
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basics.py
+++ py_client/basics.py
@@ -1,11 +1,11 @@
 import requests
 
-endpoint = 'https://httpbin.org/status/200/'
-endpoint = 'https://httpbin.org/anything'
-endpoint = 'http://localhost:8000/'
-endpoint = 'http://localhost:8000/api'
+# endpoint = 'https://httpbin.org/status/200/'
+# endpoint = 'https://httpbin.org/anything'
+# endpoint = 'http://localhost:8000/'
+endpoint = 'http://localhost:8000/api/'
 
-get_response = requests.get(endpoint , json={"query": "Hello World"}) # HTTP Request, json optional parameter will send a data object to the output
-print(get_response.text)
-print(get_response.json()['message'])
-print(get_response.status_code)
+get_response = requests.get(endpoint, params={'abc': 123},  json={"product_id": 123}) # HTTP Request, json optional parameter will send a data object to the output
+# print(get_response.text)
+print(get_response.json())
+# print(get_response.status_code)
